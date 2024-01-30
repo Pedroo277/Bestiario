0c0d845c882a1f01bde83f08c5725b81a5f9159c
Pedro Henrique <pedrogthenrique277@gmail.com>
2024-01-30 15:56:55 -0300

    up

--- bestiario/index.js
+++ bestiario/index.js
@@ -1,16 +1,16 @@
 var botao = document.getElementById("botao-alterar-tema");
 botao.addEventListener("click", function() {
-  document.body.classList.toggle("modo-escuro");
+  document.body.classList.toggle("modo-claro");
 
   var Sol = document.getElementById("imagem-botao");
   var Lua = document.getElementById("imagem-botao-2");
 
-  if (document.body.classList.contains("modo-escuro")) {
-    Sol.style.display = "none";
-    Lua.style.display = "block";
-  } else {
+  if (document.body.classList.contains("modo-claro")) {
     Sol.style.display = "block";
     Lua.style.display = "none";
+  } else {
+    Sol.style.display = "none";
+    Lua.style.display = "block";
   }
 });
 
